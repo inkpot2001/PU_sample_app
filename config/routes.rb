450192a52def5d4a48fd8f6fd692e12dc65cda9b
Rikuto Tamura <inkpot2001@gmail.com>
2023-07-14 15:13:04 +0900

    first commitment

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  root "static_pages#home"
-  get  "/help",    to: "static_pages#help"
-  get "/signup", to: "users#new"
-  get "/login", to: "sessions#new"
+  #get 'microposts/new'
+  #get 'microposts/index'
+  #get 'sessions/new'
+ # root "static_pages#home"
+  #get  "/help",    to: "static_pages#help"
+  #get "/signup", to: "users#new"
+  #get "/login", to: "sessions#new"
   #get "/microposts", to: "microposts#index"
-  post "login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
+  #post "login", to: "sessions#create"
+  #delete "/logout", to: "sessions#destroy"
+  resources :users, only: [:index, :new, :create]
+  #resources :account_activations, only: [:edit]
+  #resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:new, :create, :index, :destroy]
 end
